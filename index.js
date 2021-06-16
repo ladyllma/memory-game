6527925e43252359a2bb1a9f4787d1a20bbd32e3
Michael Litvinko <mlitvinkox96@gmail.com>
2021-06-15 20:35:45 -0400

    hours added to timer

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const timerMinutes = document.getElementById('minutes');
 const timerSeconds = document.getElementById('seconds');
+const timerHours = document.getElementById('hours');
 const resetBtn = document.getElementById('reset-button');
 
 var timerInterval;
 
+//Clicking reset button stops timer (continue button to start timer?)
 resetBtn.addEventListener('click', () => {
   stopTimer();
+  restartTimer();
 });
 
 function startTimer() {
   var seconds = 0;
   var minutes = 0;
+  var hours = 0;
+
   timerInterval = setInterval(() => {
     seconds++;
     if (seconds == 60) {
       minutes++;
       seconds = 0;
+    } 
+    if (minutes == 60) {
+      hours++;
+      minutes = 0;
     }
     timerSeconds.textContent = seconds;
     timerMinutes.textContent = minutes;
+    timerHours.textContent = hours;
   }, 1000);
 }
 
@@ -26,6 +36,11 @@ function stopTimer() {
   clearInterval(timerInterval);
   timerSeconds.textContent = 0;
   timerMinutes.textContent = 0;
+  timerHours.textContent = 0;
+}
+
+function restartTimer() {
+  startTimer();
 }
 
 startTimer();
