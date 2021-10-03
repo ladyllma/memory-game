82f5c5b0b378dcb963203b05afb7811834734d08
Leidy Laura Martinez Arias <leidylaurama@gmail.com>
2021-10-02 21:54:21 -0400

    Adding a dialog to show the score at the end of the game.

--- index.js
+++ index.js
@@ -17,13 +17,30 @@ const hoursLbl = document.getElementById('hours');
 const movesLbl = document.getElementById('moves');
 const resetBtn = document.getElementById('reset-button');
 const deck = document.getElementById('deck');
+const youRockDialog = document.getElementById('you-rock-dialog');
+const tryAgainBtn = document.getElementById('try-again-button');
+const scoreLbl = document.getElementById('score');
 
 var cards = [];
 var timerInterval;
-var moves = 0;
-var points = 0;
-//Clicking reset button stops timer (continue button to start timer?)
+const game = {
+  moves: 0,
+  points: 0,
+  timer: {
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+  },
+  score: 0,
+};
+
+// Clicking reset button restart the game
 resetBtn.addEventListener('click', () => {
+  restartGame();
+});
+
+tryAgainBtn.addEventListener('click', () => {
+  youRockDialog.close();
   restartGame();
 });
 
@@ -90,11 +107,11 @@ async function increaseMoves() {
   // Check if there are two cards selected
   if (counter == 2) {
     // Increase the moves
-    moves++;
-    movesLbl.textContent = moves;
+    game.moves++;
+    movesLbl.textContent = game.moves;
 
     // Start the timer if it is the first move
-    moves == 1 && startTimer();
+    game.moves == 1 && startTimer();
 
     // Disable all pointer events of all the
     // non-selected and non-matched cards
@@ -112,8 +129,12 @@ async function increaseMoves() {
     await new Promise((resolve, reject) => {
       // Check if the selected cards have the same icon
       if (cards[index1].iconName !== cards[index2].iconName) {
+        // cards[index1].node.classList.toggle('not-matched');
+        // cards[index2].node.classList.toggle('not-matched');
         // Flip back the cards after half a second
         setTimeout(() => {
+          // resolve(cards[index1].node.classList.toggle('not-matched'));
+          // resolve(cards[index2].node.classList.toggle('not-matched'));
           resolve(cards[index1].node.classList.toggle('flip-card'));
           resolve(cards[index2].node.classList.toggle('flip-card'));
         }, 500);
@@ -124,7 +145,7 @@ async function increaseMoves() {
         resolve((cards[index2].isMatched = true));
         resolve(cards[index2].node.classList.add('matched'));
         // Increase a point
-        resolve(points++);
+        resolve(game.points++);
       }
     });
     // Mark the cards as non-selected
@@ -138,7 +159,7 @@ async function increaseMoves() {
 
     // If the points are equal to 8, end the game
     // because all the cards are matched | (16/2 = 8)
-    points == 8 && stopGame();
+    game.points == 8 && stopGame();
   }
 }
 
@@ -148,23 +169,23 @@ function deleteCards() {
 }
 
 function startTimer() {
-  var seconds = 0;
-  var minutes = 0;
-  var hours = 0;
+  game.timer.seconds = 0;
+  game.timer.minutes = 0;
+  game.timer.hours = 0;
 
   timerInterval = setInterval(() => {
-    seconds++;
-    if (seconds == 60) {
-      minutes++;
-      seconds = 0;
+    game.timer.seconds++;
+    if (game.timer.seconds == 60) {
+      game.timer.minutes++;
+      game.timer.seconds = 0;
     }
-    if (minutes == 60) {
-      hours++;
-      minutes = 0;
+    if (game.timer.minutes == 60) {
+      game.timer.hours++;
+      game.timer.minutes = 0;
     }
-    secondsLbl.textContent = seconds;
-    minutesLbl.textContent = minutes;
-    hoursLbl.textContent = hours;
+    secondsLbl.textContent = game.timer.seconds;
+    minutesLbl.textContent = game.timer.minutes;
+    hoursLbl.textContent = game.timer.hours;
   }, 1000);
 }
 
@@ -181,10 +202,41 @@ function stopTimer() {
 
 function restartMoves() {
   cardsSelectedCounter = 0;
-  moves = 0;
-  movesLbl.textContent = moves;
+  game.moves = 0;
+  movesLbl.textContent = game.moves;
 }
 
 function stopGame() {
   stopTimer();
+
+  const { hours, minutes, seconds } = game.timer;
+
+  if (hours == 0 && minutes == 0 && seconds > 20 && seconds <= 30) {
+    console.log('+150');
+    game.score += 150;
+  } else if (hours == 0 && minutes == 0 && seconds > 10 && seconds <= 20) {
+    console.log('+200');
+    game.score += 200;
+  } else if (hours == 0 && minutes == 0 && seconds > 1 && seconds <= 10) {
+    console.log('+250');
+    game.score += 250;
+  }
+
+  if (game.moves > 24) {
+    console.log('+50');
+    game.score += 50;
+  } else if (game.moves > 8 && game.moves <= 24) {
+    console.log('+500');
+    game.score += 500;
+  } else if (game.moves == 8) {
+    console.log('+1500');
+    game.score += 1500;
+  }
+
+  console.dir(game.score);
+
+  scoreLbl.textContent = game.score;
+  setTimeout(() => {
+    youRockDialog.showModal();
+  }, 500);
 }
